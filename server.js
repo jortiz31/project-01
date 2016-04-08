1818906a885a0b1b13087ccfdad5adfd44801d6f
Jonathan <jortiz@tunein.com>
2016-04-07 18:59:20 -0700

    front end victory

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 'use strict';
-console.log('Hello World');
+var fs = require('fs');
 
 var http = require('http');
 
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!!!\n');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+  var header = 'Seaky..';
+  html = html.replace('{Header}', header);
+  res.end(html);
 
-}).listen(1337, '127.0.0.1');
+}).listen(1337);
