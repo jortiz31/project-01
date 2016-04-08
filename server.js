842c4ecc53e0d38a5bc281e9f7a0bd643c4cdde0
Jonathan <jortiz@tunein.com>
2016-04-07 19:55:43 -0700

    basic idea of routing victory

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ var fs = require('fs');
 var http = require('http');
 
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
-  var header = 'Seaky..';
-  html = html.replace('{Header}', header);
-  res.end(html);
+
+  if (req.url === '/'){
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  }
+
+  else if(req.url === '/api'){
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    var obj = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
+  }
+
+  else {
+    res.writeHead(404);
+    res.end();
+  }
 
 }).listen(1337);
