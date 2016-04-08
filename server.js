523ae22fc6deae153328ba0b7c274a51f6823ccd
Jonathan <jortiz@tunein.com>
2016-04-07 20:03:59 -0700

    basic idea of routing update

--- server.js
+++ server.js
@@ -4,23 +4,20 @@ var fs = require('fs');
 var http = require('http');
 
 http.createServer(function(req, res) {
-
   if (req.url === '/'){
     fs.createReadStream(__dirname + '/index.html').pipe(res);
   }
-
   else if(req.url === '/api'){
     res.writeHead(200, { 'Content-Type': 'application/json' });
     var obj = {
-      firstname: 'John',
-      lastname: 'Doe'
+      firstName: 'John',
+      lastName: 'Doe',
+      shoeSize: "11.5"
     };
     res.end(JSON.stringify(obj));
   }
-
   else {
     res.writeHead(404);
     res.end();
   }
-
 }).listen(1337);
