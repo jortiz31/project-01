c713fe9c136bc8ec3efc4ce8d668df51e329f917
Jonathan <jortiz@tunein.com>
2016-04-09 14:29:55 -0700

    front end page done

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+'use strict';
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 1337;
+var todos = [{
+  description: 'Meet mom for lunch',
+  completed: false,
 
-app.get('/', function(req, res){
-  res.send('Todo API Root');
-});
+}];
+
+
+app.use(express.static('public'));
+
+
+
+
+// app.get('/', function(req, res){
+//   res.send('Hello World');
+// });
 
 app.listen(PORT, function(){
   console.log('express is lisening on ' + PORT + '!');
